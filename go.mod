04519faea14fbd0612104233777f6b82b477d11e
David O'Meara <nilpunning@gmail.com>
2024-10-28 09:04:53 -0500

    Monitor for files changed.  Make index a template.

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module hallertau
 
 go 1.23.1
 
-require github.com/mattn/go-sqlite3 v1.14.16
+require (
+	github.com/fsnotify/fsnotify v1.7.0
+	github.com/mattn/go-sqlite3 v1.14.16
+)
+
+require golang.org/x/sys v0.4.0 // indirect
--- main.go
+++ main.go
@@ -4,9 +4,53 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+	"text/template"
 
+	"github.com/fsnotify/fsnotify"
 	_ "github.com/mattn/go-sqlite3"
 )
+
+var recipes map[string]bool = map[string]bool{}
+
+func monitorRecipesDirectory() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	err = watcher.Add(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			if event.Name == "recipes" {
+				if event.Has(fsnotify.Create) {
+					err = watcher.Add("recipes")
+					if err != nil {
+						log.Fatal(err)
+					}
+					recipes[event.Name] = true
+				}
+				if event.Has(fsnotify.Remove) || event.Has(fsnotify.Rename) {
+					delete(recipes, event.Name)
+				}
+			}
+			log.Println("Event:", event)
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			log.Println("Error:", err)
+		}
+	}
+}
 
 func main() {
 	// Open SQLite database
@@ -16,9 +60,24 @@ func main() {
 	}
 	defer db.Close()
 
+	go monitorRecipesDirectory()
+
 	// Serve static files
-	fs := http.FileServer(http.Dir("ui"))
+	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
+
+	// Serve the Go template
+	http.HandleFunc("/{$}", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("templates/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
 
 	// Start the web server
 	log.Println("Server starting on http://localhost:8080")
@@ -26,4 +85,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
